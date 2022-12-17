80f48e42322a2e93aa9662e9b646d8c14712a49d
Dmitri <dmitri.antipov1@gmail.com>
2022-12-17 12:55:04 +0200

    create layout components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { ContentComponent } from './components/layout/content/content.component';
+import { ProductsComponent } from './components/layout/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
